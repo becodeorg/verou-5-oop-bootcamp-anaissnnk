6bdec9ee227f49857d9a7207e539e15ec9fddc19
Anaïs <aekyon.08@gmail.com>
2024-01-10 11:53:33 +0100

    Add 4th exercise

---
+++ Use Case 4/index.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+There's two groups, both of 10 students.
+Every student has a name (even Jaqen) and gets a grade.
+You can have some fun coming up with the content here :-)
+
+Provide an easy way to calculate the average score of a group.
+Add a function to move a student from one group to another.
+Show the average score of both groups.
+Move the top student from one group with the lowest scoring student from another.
+Show the averages again - how were these affected?*/
